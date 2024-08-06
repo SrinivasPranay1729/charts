ae9fd5a15f5df4c1ad249a0ba6f7b8d866b614e2
skully-lamp <151653934+skully-lamp@users.noreply.github.com>
2024-08-06 14:37:35 -0400

    Update rxpor.py

--- rxpor.py
+++ rxpor.py
@@ -22,6 +22,60 @@ TASK_FAIL_SELECT = "select tf.task_id, tf.dag_id, tf.start_date, tf.end_date, tf
 JOB_SELECT = "select dag_id,  state, job_type , start_date, end_date, latest_heartbeat, executor_class, hostname, unixname from job"
 POOL_SLOTS = "select pool, slots, description from slot_pool where pool != 'default_pool'"
 
+
+
+
+# SQL Queries with corrected column references
+DAG_RUN_SELECT = """
+select dag_id, execution_date, state, run_id, external_trigger,
+'\\x' || encode(conf, 'hex') as conf, end_date, start_date, run_type,
+last_scheduling_decision, dag_hash, creating_job_id, null as queued_at,
+null as data_interval_start, null as data_interval_end 
+from dag_run
+"""
+
+TASK_INSTANCE_SELECT = """
+select ti.task_id, ti.dag_id, ti.start_date, ti.end_date, ti.duration,
+ti.state, ti.try_number, ti.hostname, ti.unixname, ti.job_id, ti.pool,
+ti.queue, ti.priority_weight, ti.operator, ti.queued_dttm, ti.pid, ti.max_tries,
+'\\x' || encode(ti.executor_config, 'hex') as executor_config, ti.pool_slots,
+ti.queued_by_job_id, ti.external_executor_id, null as trigger_id, null as trigger_timeout,
+null as next_method, null as next_kwargs, r.run_id as run_id 
+from task_instance ti, dag_run r 
+where r.dag_id = ti.dag_id AND r.run_id = ti.run_id
+"""
+
+LOG_SELECT = """
+select dttm, dag_id, task_id, event, execution_date, owner, extra 
+from log
+"""
+
+TASK_FAIL_SELECT = """
+select tf.task_id, tf.dag_id, tf.start_date, tf.end_date, tf.duration,
+-1 as map_index, r.run_id 
+from task_fail tf, dag_run r 
+where r.dag_id = tf.dag_id AND r.execution_date = tf.execution_date
+"""
+
+JOB_SELECT = """
+select dag_id, state, job_type, start_date, end_date, latest_heartbeat,
+executor_class, hostname, unixname 
+from job
+"""
+
+POOL_SLOTS = """
+select pool, slots, description 
+from slot_pool where pool != 'default_pool'
+"""
+
+OBJECTS_TO_EXPORT = [
+    [DAG_RUN_SELECT, "dag_run"],
+    [TASK_INSTANCE_SELECT, "task_instance"],
+    [LOG_SELECT, "log"],
+    [TASK_FAIL_SELECT, "task_fail"],
+    [JOB_SELECT, "job"],
+    [POOL_SLOTS, "slot_pool"],
+]
 OBJECTS_TO_EXPORT = [
     [DAG_RUN_SELECT, "dag_run"],
     [TASK_INSTANCE_SELECT, "task_instance"],
